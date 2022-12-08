82f809c258dd866dd83b34aabb295c8c07cccc15
Maxainn21 <114251580+Maxainn21@users.noreply.github.com>
2022-12-08 23:45:23 +0700

    denis ganteng

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Galonline } from "../../assets/Galonline.svg";
+import { ReactComponent as Search } from "../../assets/search.svg";
+import { ReactComponent as Profile } from "../../assets/profile.svg";
+import { ReactComponent as Cart } from "../../assets/cart.svg";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -7,7 +11,7 @@ function Navbar() {
   return (
     <>
       <nav className="w-full bg-[#393E46]  ">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               <div className="relative w-full lg:max-w-sm">
@@ -20,14 +24,16 @@ function Navbar() {
               </div>
               <div className="md:hidden">
                 <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}>
+                  className="p-2 text-white rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
                   {navbar ? (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6 "
                       viewBox="0 0 20 20"
-                      fill="currentColor">
+                      fill="currentColor"
+                    >
                       <path
                         fillRule="evenodd"
                         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -41,7 +47,8 @@ function Navbar() {
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
-                      strokeWidth={2}>
+                      strokeWidth={2}
+                    >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
@@ -56,38 +63,28 @@ function Navbar() {
           <div>
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'block' : 'hidden'
-              }`}>
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="hover:text-blue-500">
-                  <a href="/">Home</a>
-                </li>
-                <li className="hover:text-blue-500">
-                  <a href="#services">Services</a>
-                </li>
-                <li className="hover:text-blue-500">
-                  <a href="#review">Review</a>
-                </li>
-                <li className="hover:text-blue-500">
-                  <a href="#about-us">About Us</a>
-                </li>
+                navbar ? "block" : "hidden"
+              }`}
+            >
+              <ul className="items-center justify-center space-y-8 mt-8 md:flex md:space-x-24 md:space-y-0">
+                <Galonline />
+                <img src="" alt="" />
               </ul>
 
               <div className="mt-3 space-y-2 lg:hidden md:inline-block">
                 <Link
-                  to={'/sign-in'}
-                  className="w-full inline-block px-6 py-2 border-2 border-black font-medium text-xs leading-normal uppercase rounded hover:bg-blue-400 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
+                  to={"/sign-in"}
+                  className="w-full inline-block px-6 py-2 border-2 border-black font-medium text-xs leading-normal uppercase rounded hover:bg-blue-400 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                >
                   Sign in
                 </Link>
               </div>
             </div>
           </div>
-          <div className="hidden space-x-2 md:inline-block">
-            <Link
-              to="/sign-in"
-              className="px-8 py-3 outline outline-1 hover:bg-blue-400 rounded-md">
-              Sign in
-            </Link>
+          <div className="flex space-x-2 md:inline-block ">
+            <Search />
+            <Profile />
+            <Cart />
           </div>
         </div>
       </nav>
