ddb30eb8ac0dfbfbf81a6d46f136f315953907ba
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-12-14 21:00:01 +0700

    push them

--- src/parts/Service.js
+++ src/parts/Service.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React from 'react';
 
 export default function Service() {
   return (
-    <>
-      <div class="container">
-        <div class="bg">
-          <img src="https://unsplash.it/400/200" alt="" />
-          <div class="overlay">
-            <h2>
-              Check This <span>Out!</span>
-            </h2>
-            <p>this is some text.</p>
-          </div>
-        </div>
+    <section style={{ maxWidth: '900px' }} className="flex container mx-auto">
+      <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+        <picture class="rounded-lg block overflow-hidden">
+          <img class="hover:scale-125" src="/galon1.png" alt="" />
+        </picture>
+
+        <h1 class="mt-4 mb-2 text-xl font-bold">Judul Card di sini</h1>
+        <p class="text-sm text-gray-600">
+          Keterangan pendek tentang card di sini. Biasanya panjangnya dua sampai
+          tiga baris, biar terlihat rapi.
+        </p>
       </div>
-    </>
+
+      <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+        <picture class="rounded-lg block overflow-hidden">
+          <img
+            class="hover:scale-125"
+            src="https://placeimg.com/360/240/tech"
+            alt=""
+          />
+        </picture>
+
+        <h1 class="mt-4 mb-2 text-xl font-bold">Judul Card di sini</h1>
+        <p class="text-sm text-gray-600">
+          Keterangan pendek tentang card di sini. Biasanya panjangnya dua sampai
+          tiga baris, biar terlihat rapi.
+        </p>
+      </div>
+    </section>
   );
 }
