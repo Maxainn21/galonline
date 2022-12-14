7be7545fdffbfe7e0879a3116a5582e83774a15d
Maxainn21 <114251580+Maxainn21@users.noreply.github.com>
2022-12-14 19:16:25 +0700

    ahah ah ah

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./parts/Logform";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/home" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
---
+++ src/parts/Service.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Service() {
+  return (
+    <div>
+      <div class="container">
+        <div class="bg">
+          <img src="https://unsplash.it/400/200" alt="" />
+          <div class="overlay">
+            <h2>
+              Check This <span>Out!</span>
+            </h2>
+            <p>this is some text.</p>
+          </div>
+        </div>
+        <div class="bg">
+          <img src="https://unsplash.it/400/200" alt="" />
+          <div class="overlay">
+            <h2>
+              Check This <span>Out!</span>
+            </h2>
+            <p>this is some text.</p>
+          </div>
+        </div>
+        <div class="bg">
+          <img src="https://unsplash.it/400/200" alt="" />
+          <div class="overlay">
+            <h2>Check This Out!</h2>
+            <p>this is some text.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
