6218d224661eb29db2e52fbb92ababf2f6cc9940
Maxainn21 <114251580+Maxainn21@users.noreply.github.com>
2022-12-14 23:38:53 +0700

    okoklh

--- src/parts/Service.js
+++ src/parts/Service.js
@@ -5,41 +5,43 @@ export default function Service() {
     {
       id: 1,
       image: "/galon1.png",
+      title: "Beli Galon",
     },
     {
       id: 2,
       image: "/galon2.png",
+      title: "Isi Ulang Galon",
     },
   ];
   return (
-    <section
-      style={{ maxWidth: "900px" }}
-      className="flex flex-wrap container mx-auto"
-    >
-      {data.map((item) => {
-        return (
-          <div
-            key={item.id}
-            style={{ width: "350px" }}
-            className="p-4 m-8 w-80 bg-white shadow-md rounded-xl"
-          >
-            <picture className="rounded-lg block overflow-hidden">
-              <img
-                style={{ width: "400px", height: "200px" }}
-                className="hover:scale-125"
-                src={item.image}
-                alt=""
-              />
-            </picture>
+    <>
+      {" "}
+      <p className="text-center mt-10 text-2xl font-semibold">LAYANAN KAMI</p>
+      <section
+        style={{ maxWidth: "900px" }}
+        className="flex flex-wrap container mx-auto"
+      >
+        {data.map((item) => {
+          return (
+            <div
+              key={item.id}
+              style={{ width: "350px" }}
+              className="p-4 m-8 w-80 bg-white shadow-md hover:shadow-2xl rounded-xl"
+            >
+              <picture className="rounded-lg block overflow-hidden">
+                <img
+                  style={{ width: "400px", height: "200px" }}
+                  className="hover:scale-125"
+                  src={item.image}
+                  alt=""
+                />
+              </picture>
 
-            <h1 className="mt-4 mb-2 text-xl font-bold">Judul Card di sini</h1>
-            <p className="text-sm text-gray-600">
-              Keterangan pendek tentang card di sini. Biasanya panjangnya dua
-              sampai tiga baris, biar terlihat rapi.
-            </p>
-          </div>
-        );
-      })}
-    </section>
+              <h1 className="mt-4 mb-2 text-xl font-bold">{item.title}</h1>
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
 }
