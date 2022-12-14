61dd6d25321b8b50681c473af7cd92e85702e0a7
Maxainn21 <114251580+Maxainn21@users.noreply.github.com>
2022-12-14 22:39:26 +0700

    anjai

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../parts/Hero';
-import Brands from '../parts/Brands';
-import Service from '../parts/Service';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../parts/Hero";
+import Brands from "../parts/Brands";
+import Service from "../parts/Service";
+import Info from "../parts/Info";
+import Footer from "../parts/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <Hero />
       <Brands />
       <Service />
+      <Info />
+      <Footer />
     </>
   );
 }
--- src/parts/Service.js
+++ src/parts/Service.js
@@ -1,30 +1,45 @@
 import React from "react";
 
 export default function Service() {
+  const data = [
+    {
+      id: 1,
+      image: "/galon1.png",
+    },
+    {
+      id: 2,
+      image: "/galon2.png",
+    },
+  ];
   return (
-    <section style={{ maxWidth: "900px" }} className="flex container mx-auto">
-      <div class="container">
-        <img src="" alt="Avatar" class="image" style="width:100%" />
-        <div class="middle">
-          <div class="text">John Doe</div>
-        </div>
-      </div>
+    <section
+      style={{ maxWidth: "900px" }}
+      className="flex flex-wrap container mx-auto"
+    >
+      {data.map((item) => {
+        return (
+          <div
+            key={item.id}
+            style={{ width: "350px" }}
+            className="p-4 m-8 w-80 bg-white shadow-md rounded-xl"
+          >
+            <picture className="rounded-lg block overflow-hidden">
+              <img
+                style={{ width: "400px", height: "200px" }}
+                className="hover:scale-125"
+                src={item.image}
+                alt=""
+              />
+            </picture>
 
-      <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-        <picture class="rounded-lg block overflow-hidden">
-          <img
-            class="hover:scale-125"
-            src="https://placeimg.com/360/240/tech"
-            alt=""
-          />
-        </picture>
-
-        <h1 class="mt-4 mb-2 text-xl font-bold">Judul Card di sini</h1>
-        <p class="text-sm text-gray-600">
-          Keterangan pendek tentang card di sini. Biasanya panjangnya dua sampai
-          tiga baris, biar terlihat rapi.
-        </p>
-      </div>
+            <h1 className="mt-4 mb-2 text-xl font-bold">Judul Card di sini</h1>
+            <p className="text-sm text-gray-600">
+              Keterangan pendek tentang card di sini. Biasanya panjangnya dua
+              sampai tiga baris, biar terlihat rapi.
+            </p>
+          </div>
+        );
+      })}
     </section>
   );
 }
