244215e942a94fc63829e079fd1e2a8629c03870
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2022-12-19 23:25:01 +0700

    search page

--- src/parts/Search.js
+++ src/parts/Search.js
@@ -2,38 +2,37 @@ import React from "react";
 
 export default function Search() {
   return (
-    <div class="bg-[#222831] min-h-screen">
-      <div class="max-w-md mx-auto my-auto">
-        <div
-          class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden"
-          style={{ margin: "0 auto" }}
-        >
-          <div class="grid place-items-center h-full w-12 text-gray-300">
+    <div className="bg-[#222831] min-h-screen">
+      <div className="text-white text-5xl flex items-center justify-center"></div>
+
+      <form className="container mx-auto flex flex-col items-center justify-center h-screen">
+        <img className="mb-10" src="/galonsearchlogo.png" alt="" />
+
+        <div className="relative w-full">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
             </svg>
           </div>
-
           <input
-            class="peer h-full w-full outline-none text-sm text-gray-700 pr-2 "
             type="text"
-            id="searchs"
-            placeholder="Search something.."
+            id="simple-search"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Cari produk yang kamu inginkan"
+            required
           />
         </div>
-      </div>
-      ;
+      </form>
     </div>
   );
 }
